7b9c97d60b22583d7aa9909ec52e149a932f83df
David <david.kras05@gmail.com>
2026-04-09 20:11:32 -0700

    fixed indentation

--- register_file/registerfile_top.sv
+++ register_file/registerfile_top.sv
@@ -7,25 +7,25 @@ module registerfile_top(
 	
 );
 
-logic [31:0] dec_out;
+	logic [31:0] dec_out;
 
-five_thirtytwodecoder dec (.A(reg_id[4]),
-                           .B(reg_id[3]),
-									.C(reg_id[2]),
-									.D(reg_id[1]),
-									.E(reg_id[0]),
-									.En(write_en),
-						   .y(dec_out));
+	five_thirtytwodecoder dec (.A(reg_id[4]),
+										.B(reg_id[3]),
+										.C(reg_id[2]),
+										.D(reg_id[1]),
+										.E(reg_id[0]),
+										.En(write_en),
+										.y(dec_out));
 			
-logic [31:0][63:0] reg_outs;
+	logic [31:0][63:0] reg_outs;
 			
-genvar i;
+	genvar i;
 
-generate
-	for (i = 0; i<31; i++) begin: register_loop
-		register registers (.clk(clk), .write(write), .reset(reset), .q(reg_outs[i]));
-	end
-endgenerate
+	generate
+		for (i = 0; i<31; i++) begin: register_loop
+			register registers (.clk(clk), .write(write), .reset(reset), .q(reg_outs[i]));
+		end
+	endgenerate
 
 	loopedthirtytwo_mux mux0 (.data_lines(reg_outs), .s(read_reg_0), .mux_out(regout_0));
 	loopedthirtytwo_mux mux1 (.data_lines(reg_outs), .s(read_reg_1), .mux_out(regout_1));
