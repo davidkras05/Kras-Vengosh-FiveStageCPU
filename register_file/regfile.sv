372e36f6ebb718047b7d4a1f5d0c8be626928087
David <david.kras05@gmail.com>
2026-04-10 13:29:52 -0700

    added timescale in top level

--- register_file/regfile.sv
+++ register_file/regfile.sv
@@ -1,3 +1,5 @@
+`timescale 1ns/10ps
+
 module regfile(
 	output logic [63:0] ReadData1, ReadData2, //These were originally 32 bit vectors but I think they are supposed to be 64 since they are outputs of the registers
 	input logic [63:0] WriteData,
