0a12c053db02c192658263feb69c6ea47467ba87
avengosh <avengosh@gmail.com>
2026-05-03 17:41:54 -0700

    Made variable-width mux, and wrote EX file.

---
+++ Single-Cycle/EX.sv
@@ -0,0 +1,29 @@
+module EX (
+	input logic UncondBr,
+	input logic [3:0] ALUOp,
+	input logic [31:0] instruction,
+	input logic [63:0] ALUIn0, ALUIn1,
+	output logic [63:0] ALURes, BrLoc,
+	output logic ZeroFlag
+);
+
+	ALU ALU (.A(ALUIn0), .B(ALUIn1), .cntrl(), .result(ALURes), .negative(), .zero(ZeroFlag), .overflow(), .carry_out()) //NEED TO FIGURE OUT CNTRL
+
+	logic [18:0] CondAddr19;
+	logic [25:0] BrAddr26;
+
+	assign CondAddr19 = instruction[23:5];
+	assign BrAddr26 = instruction[25:0];
+
+	logic [63:0] BrAddr64, CondAddr64;
+
+	assign BrAddr64 = {{38{BrAddr26[25]}}, BrAddr26};
+	assign CondAddr64 = {{45{CondAddr19[25]}}, CondAddr19};
+
+	logic [63:0] BrLoc_unshifted;
+
+	n_bit_2to1 #(.BITS(64)) (.data_line_1(BrAddr64), .data_line_0(CondAddr64), .s(UncondBr), .mux_out(BrLoc_unshifted))
+
+	assign BrLoc = BrLoc_unshifted << 2;
+
+endmodule 
--- Single-Cycle/IF.sv
+++ Single-Cycle/IF.sv
@@ -1,13 +1,13 @@
 module IF (
 	input clk, reset,
-	input logic[63:0] Branchloc, //64 bits because PC register is 64 bits
+	input logic[63:0] BrLoc, //64 bits because PC register is 64 bits
 	input logic BrTaken,
 	output logic[31:0] instruction_output;
 );
 
 	logic[63:0] adderwire, curr_PC, next_PC;
 
-	sixty_four_2to1 branchMux (.data_line1(Branchloc), .data_line0(64'b4), .s(BrTaken), .mux_out(adder_wire));
+	n_bit_2to1 #(.BITS(64)) branchMux (.data_line1(BrLoc), .data_line0(64'b4), .s(BrTaken), .mux_out(adder_wire));
 		
 	sixtyfourbit_fulladder PCadd (.A(adder_wire), .B(curr_PC), .Cin(1'b0), .S(next_PC));
 	
