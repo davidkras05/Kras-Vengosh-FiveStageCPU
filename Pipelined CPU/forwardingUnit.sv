07982843745c80ff9d7c860c8d94b511e942dab3
David <david.kras05@gmail.com>
2026-05-16 18:05:47 -0700

    created skeleton for the forwarding unit

---
+++ Pipelined CPU/forwardingUnit.sv
@@ -0,0 +1,10 @@
+module forwardingUnit (
+	input logic[4:0] Rn, Rm, RdEXMEM, RdMEMWB,
+	input logic MemtoRegEXMEM, RegWriteEXMEM, MemtoRegMEMWB, RegWriteMEMWB,
+	
+	output logic[1:0] ForwardA, ForwardB
+);
+	
+	//Compare next instruction with current instruction logic.
+
+endmodule
