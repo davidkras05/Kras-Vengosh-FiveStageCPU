e6e468ef251a0fa33f6f9925a860ee6f44201825
avengosh <avengosh@gmail.com>
2026-05-18 21:44:37 -0700

    wiring fwding unit

--- Pipelined CPU/forwardingUnit.sv
+++ Pipelined CPU/forwardingUnit.sv
@@ -8,19 +8,20 @@
 	output logic[1:0] ForwardA, ForwardB
 );
 
+
 	parameter DELAY = 50;
 	
 	//Compare next instruction with current instruction logic.
 	
 	//Equality wires:
-	logic RdEXMEM_eq_Rn, RdEXMEM_eq_Rm;
-	logic RdMEMWB_eq_Rn, RdMEMWB_eq_Rm;
+	logic[5:0] RdEXMEM_eq_Rn, RdEXMEM_eq_Rm;
+	logic[5:0] RdMEMWB_eq_Rn, RdMEMWB_eq_Rm;
 	
-	assign RdEXMEM_eq_Rn = RegWriteEXMEM;
-	assign RdEXMEM_eq_Rm = RegWriteEXMEM;
+	assign RdEXMEM_eq_Rn[0] = RegWriteEXMEM;
+	assign RdEXMEM_eq_Rm[0] = RegWriteEXMEM;
 	
-	assign RdMEMWB_eq_Rn = RegWriteMEMWB;
-	assign RdMEMWB_eq_Rm = RegWriteMEMWB;
+	assign RdMEMWB_eq_Rn[0] = RegWriteMEMWB;
+	assign RdMEMWB_eq_Rm[0] = RegWriteMEMWB;
 	
 	//Create intermediate wires (signal_store is for the equality checking bit, signal_store_1 is for checking that Rd is not X31)
 	
@@ -35,22 +36,22 @@
 		   //0-4
 			xnor #(DELAY) (eq_store[i], RdEXMEM[i], RnIDEX[i]);
 			not #(DELAY) (x31_store[i], RdEXMEM[i]);
-			and #(DELAY) (RdEXMEM_eq_Rn, RdEXMEM_eq_Rn, eq_store[i], x31_store[i]);
+			and #(DELAY) (RdEXMEM_eq_Rn[i+1], RdEXMEM_eq_Rn[i], eq_store[i], x31_store[i]);
 			
 			//5-9
 			xnor #(DELAY) (eq_store[i+5], RdEXMEM[i+5], RmIDEX[i]);
 			not #(DELAY) (x31_store[i+5], RdEXMEM[i+5]);
-			and #(DELAY) (RdEXMEM_eq_Rm, RdEXMEM_eq_Rm, eq_store[i+5], x31_store[i+5]);
+			and #(DELAY) (RdEXMEM_eq_Rm[i+1], RdEXMEM_eq_Rm[i], eq_store[i+5], x31_store[i+5]);
 			
 			//10-14
 			xnor #(DELAY) (eq_store[i+10], RdMEMWB[i+10], RnIDEX[i]);
 			not #(DELAY) (x31_store[i+10], RdMEMWB[i+10]);
-			and #(DELAY) (RdMEMWB_eq_Rn, RdMEMWB_eq_Rn, eq_store[i+10], x31_store[i+10]);
+			and #(DELAY) (RdMEMWB_eq_Rn[i+1], RdMEMWB_eq_Rn[i], eq_store[i+10], x31_store[i+10]);
 			
 			//15-19
 			xnor #(DELAY) (eq_store[i+15], RdMEMWB[i+15], RmIDEX[i]);
 			not #(DELAY) (x31_store[i+15], RdMEMWB[i+15]);
-			and #(DELAY) (RdMEMWB_eq_Rm, RdMEMWB_eq_Rm, eq_store[i+15], x31_store[i+15]);
+			and #(DELAY) (RdMEMWB_eq_Rm[i+1], RdMEMWB_eq_Rm[i], eq_store[i+15], x31_store[i+15]);
 		end
 	endgenerate
 	
@@ -58,15 +59,15 @@
 	// EXMEM fwding takes precedence, so must make sure 2nd bit is secondary to first bit
 	
 	logic MEMWB_allowed_A, MEMWB_allowed_B;
-	not #(DELAY) (MEMWB_allowed_A, RdEXMEM_eq_Rn);
-	not #(DELAY) (MEMWB_allowed_B, RdEXMEM_eq_Rm);
+	not #(DELAY) (MEMWB_allowed_A, RdEXMEM_eq_Rn[5]);
+	not #(DELAY) (MEMWB_allowed_B, RdEXMEM_eq_Rm[5]);
 	
 	logic MEMWB_needed_A, MEMWB_needed_B;
-	and #(DELAY) (MEMWEB_needed_A, MEMWB_allowed_A, RdMEMWB_eq_Rn);
-	and #(DELAY) (MEMWEB_needed_A, MEMWB_allowed_A, RdMEMWB_eq_Rn);
+	and #(DELAY) (MEMWB_needed_A, MEMWB_allowed_A, RdMEMWB_eq_Rn[5]);
+	and #(DELAY) (MEMWB_needed_B, MEMWB_allowed_B, RdMEMWB_eq_Rm[5]);
 	
-	assign ForwardA = {RdEXMEM_eq_Rn, MEMWB_needed_A};
-	assign ForwardB = {RdEXMEM_eq_Rm, MEMWB_needed_B};
+	assign ForwardA = {RdEXMEM_eq_Rn[5], MEMWB_needed_A};
+	assign ForwardB = {RdEXMEM_eq_Rm[5], MEMWB_needed_B};
 	
 
 endmodule
